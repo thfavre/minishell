225223743ac72933b5bd22ad364a9c963ca801e3
thfavre <thomas.favre22@yahoo.fr>
2023-03-16 17:44:07 +0100

    getenv patached

--- srcs/environment/ft_getenv.c
+++ srcs/environment/ft_getenv.c
@@ -12,7 +12,12 @@ char	*ft_getenv(char **env, char *key)
 		key_end_index = 0;
 		while (env[i][key_end_index] && env[i][key_end_index] != '=')
 			key_end_index++;
-		if (ft_strncmp(env[i], key, key_end_index) == 0)
+		if (ft_strlen(key) != key_end_index || env[i][key_end_index] == '\0')
+		{
+			i++;
+			continue ;
+		}
+		else if (ft_strncmp(env[i], key, key_end_index) == 0)
 			return (env[i] + key_end_index + 1);
 		i++;
 	}
