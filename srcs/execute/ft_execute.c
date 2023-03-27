a566d1ec42741c6d107ce2f258719e06048c61ad
MrMobbi <julliatmathias@gmail.com>
2023-03-27 15:59:13 +0200

    bug fixed

--- srcs/execute/ft_execute.c
+++ srcs/execute/ft_execute.c
@@ -4,7 +4,6 @@
 void ft_execute(t_minishell *ms)
 {	
 	// if only one command => No need of a fork for ONLY the builtins ^^
-	printf("{%s}\n", ms->cmd->option[1]);
 	if (ms->cmd->next == NULL)
 		ft_execute_cmd(ms);
 	else
