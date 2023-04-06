8e584e50b70b05831ed8ad3a2712c7ebc11c64d1
MrMobbi <julliatmathias@gmail.com>
2023-04-06 15:24:41 +0200

    cat | ls corriged

--- srcs/execute/ft_execute_cmds.c
+++ srcs/execute/ft_execute_cmds.c
@@ -10,13 +10,30 @@ void	ft_execute_cmds(t_minishell *ms)
 	int fd_pipe[2];
 	int exit_status;
 
+
+	// while (waitpid(-1, NULL, WUNTRACED) == -1)
+	// 	;
+	// wait for all child processes to finish
 	while (cmd)
 	{
 		pipe(fd_pipe);
 		pid_t fork_pid = fork();
 		if (fork_pid == 0) // child
 		{
-			if (cmd->fd_read)
+			// read
+			close(fd_pipe[0]);
+			if (cmd->fd_read == 0)
+				cmd->fd_read = fd_pipe_read_tmp;
+			dup2(cmd->fd_read, 0);
+
+			// write
+			if (cmd->fd_write >= 3)
+				close(fd_pipe[1]);
+			else if (!cmd->next)
+				cmd->fd_write = 1;
+			else
+				cmd->fd_write = fd_pipe[1];
+			dup2(cmd->fd_write, 1); // write
 			ft_run_cmd(ms, cmd);
 		}
 		// parent
@@ -29,31 +46,7 @@ void	ft_execute_cmds(t_minishell *ms)
 			close(cmd->fd_read);
 		fd_pipe_read_tmp = fd_pipe[0];
 		cmd = cmd->next;
-	/*	{
-			// read
-			if (cmd->fd_read >= 3)
-				close(fd_pipe[0]);
-			else
-				cmd->fd_read = fd_pipe_read_tmp;
-			dup2(cmd->fd_read, 0);
-
-			// write
-			if (cmd->fd_write >= 3)
-					close(fd_pipe[1]);
-			else if (!cmd->next)
-				cmd->fd_write = 1;
-			else
-				cmd->fd_write = fd_pipe[1];
-			dup2(cmd->fd_write, 1); // write
-		}*/
 	}
-	close(fd_pipe[0]);
-	close(fd_pipe[1]);
-
-	// while (waitpid(-1, NULL, WUNTRACED) == -1)
-	// 	;
-	// wait for all child processes to finish
-
 	while (waitpid(-1, &exit_status, 0) > 0)
 		;
 	// printf("status : %d\n", WEXITSTATUS(status));
